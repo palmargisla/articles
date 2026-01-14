d361e31d40f8e69436d6b1c0161d086bcc4e787b
Palmar macbook <palmargisla@gmail.com>
2026-01-14 22:08:16 +0000

    Updating data for 2026

--- realestate-market-jan2026/core.py
+++ realestate-market-jan2026/core.py
@@ -1,4 +1,4 @@
 
 from analytiq.database.postgres import PostgresConnection
     
-db = PostgresConnection("postgres", "YENTKzqADVFz1EgimOWGG3fzjaH7k4YnXMA76gqbHIA=", "postgres", "127.0.0.1", 15432)
+db = PostgresConnection("postgres", "", "postgres", "127.0.0.1", 15432)
